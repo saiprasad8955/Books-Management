1cf655d6f661a621d4431722634849138bc74f7f
saiprasad <rsaiprasad4@gmail.com>
2022-07-10 21:01:04 +0530

    ENV FILE ADDED

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const route = require("./route/route");
 const bodyParser = require("body-parser")
@@ -7,16 +8,14 @@ const mongoose = require("mongoose")
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect(
-    "mongodb+srv://Sai0047:rXxgqYKPqwnhcXX7@cluster0.qptsw.mongodb.net/group23DatabaseBook"
-,{useNewUrlParser:true})
-.then(()=>{console.log(chalk.bgCyan("MongoDB is Successfully Connected"))})
-.catch((err)=> {console.log(err.message)});
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+    .then(() => { console.log(chalk.bgCyan("MongoDB is Successfully Connected")) })
+    .catch((err) => { console.log(err.message) });
 
-app.use('/',route);
+app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function () {
     console.log(chalk.bgCyan("😍Express app running on port") + (process.env.PORT || chalk.bgCyan("3000")));
-  });
+});

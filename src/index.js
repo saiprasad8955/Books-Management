75f64ee71fe2fd14f41d2d3302a78f25da32bfb0
saiprasad <rsaiprasad4@gmail.com>
2022-09-12 19:57:30 +0530

    refactored

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 require('dotenv').config()
 const express = require("express");
 const route = require("./route/route");
-const bodyParser = require("body-parser")
 const app = express();
-const chalk = require("chalk");
+
 const mongoose = require("mongoose")
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
-    .then(() => { console.log(chalk.bgCyan("MongoDB is Successfully Connected")) })
+    .then(() => { console.log("MongoDB is Successfully Connected") })
     .catch((err) => { console.log(err.message) });
 
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function () {
-    console.log(chalk.bgCyan("😍Express app running on port") + (process.env.PORT || chalk.bgCyan("3000")));
+    console.log("😍Express app running on port" + (process.env.PORT ||"3000"));
 });
